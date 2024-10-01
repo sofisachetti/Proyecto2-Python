5c470e6e8330177b898b13efc534676b4c3fa388
kprieto <anakarenprieto@live.com>
2024-09-30 18:49:48 -0700

    Añadi el metodo cambiar_catalogo y modifique eliminar_catalogo,menu_opciones

--- catalogo_peliculas.py
+++ catalogo_peliculas.py
@@ -29,6 +29,7 @@ class CatalogoPelicula:
         
     #Permite ingresar la información del nombre del catálogo de películas
     def datos_catalogo():
+        print("--- CREACIÓN DEL CATÁLOGO ---")
         nombreCat = input("Escribe el nombre del catálogo de películas: ").title()
         catalogoPeli = CatalogoPelicula(nombreCat)  
         catalogoPeli.creacion_catalogo()
@@ -40,7 +41,7 @@ class CatalogoPelicula:
         clasificacionPelicula = input(f"Escribe la clasificación de la película ({nombrePelicula}): ").title()
         peli = pelicula.Pelicula(nombrePelicula,clasificacionPelicula)
         resultado = peli.buscar_pelicula(self.ruta_archivo_completo, nombrePelicula)
-        if resultado:
+        if resultado == False:
             with open(self.ruta_archivo_completo, "a", encoding="utf-8") as file:
                 file.write(f"\n{peli.get_nombre()} - {peli.clasificacion}")
             print(f"La película {peli.get_nombre()} se registro exitosamente.")
@@ -49,31 +50,51 @@ class CatalogoPelicula:
 
     # Funcion para listar todas las peliculas dentro de un catalogo buscado
     def listar_peliculas(self):
-        print("---MOSTRAR PELICULAS---")
-        catalogoListar = input("Escribe el nombre del catalogo: ").title()
+        print("---MOSTRAR PELÍCULAS---")
+        catalogoListar = input("Escribe el nombre del catálogo: ").title()
         if catalogoListar == self.nombreCatalogo:
-            with open(self.ruta_archivo_completo, 'r') as file:
-                print(f"Peliculas del catalogo {catalogoListar}: \n")
+            with open(self.ruta_archivo_completo, 'r', encoding="utf-8") as file:
+                print(f"Películas del catálogo {catalogoListar}: \n")
                 contenidoCatalogo = file.read()
                 print(contenidoCatalogo)
         else:
-            print("Lo sentimos. El catalogo ingresado no existe o esta escrito de forma incorrecta.")
+            print("Lo sentimos. El catálogo ingresado no existe o esta escrito de forma incorrecta.")
     
     # Funcion que permite al usuario eliminar un catalogo si así lo desea
     def eliminar_catalogo(self):
         print("--- ELIMINAR CATÁLOGO ---")
-        catalogoAEliminar = input("Ingrese el nombre del catalogo que desee eliminar: ").title()
-        confirmacion = input(f'¿Está seguro que desea borrar el catálogo {catalogoAEliminar} de forma permanente? (Si/No): ').title()
+        catalogoAEliminar = input("Ingrese el nombre del catálogo que desee eliminar: ").title()
+        confirmacion = ""
+        while confirmacion not in ["Si", "No"]:
+            confirmacion = input(f'¿Está seguro que desea borrar el catálogo {catalogoAEliminar} de forma permanente? (Si/No): ').title()
+            if confirmacion not in ["Si", "No"]:
+                print("Error: Solo se permite una opción de Si o No.")
+        
         if confirmacion == 'Si' and catalogoAEliminar == self.nombreCatalogo:
             if os.path.exists(self.ruta_archivo_completo):
                 os.remove(self.ruta_archivo_completo)
-                print(f"El catalogo {catalogoAEliminar} se elimino con exito.")
+                print(f"El catálogo {catalogoAEliminar} se elimino con exito.")
+                print("Crea un nuevo catálogo de películas:")
+                return CatalogoPelicula.datos_catalogo()
             else:
-                print(f"El catalogo {catalogoAEliminar} no existe. Asegurese de haber escrito bien el nombre.")
+                print(f"El catálogo {catalogoAEliminar} no existe. Asegurese de haber escrito bien el nombre.")
         elif confirmacion == 'No':
-            print("Ha seleccionado no eliminar el catalogo.")
-        else: 
-            print("Opcion no valida. Debe responder con Si o No.")            
+            print("Ha seleccionado no eliminar el catálogo.")
+
+    #Permite la opción de cambiar a otro catálogo de películas
+    def cambiar_catalogo(self):
+        print("--- CAMBIAR CATÁLOGO ---")
+        confirmacion = ""
+        while confirmacion not in ["Si", "No"]:
+            confirmacion = input(f"¿Quieres cambiar a otro catálogo de películas? (Si/No): ").title()
+            if confirmacion not in ["Si", "No"]:
+                print("Error: Solo se permite una opción de Si o No.")
+            
+        if confirmacion == "Si":
+            print("Haz elegido cambiar de catálogo de películas.")
+            return CatalogoPelicula.datos_catalogo()
+        else:
+            print(f"Haz regresado al menú del catálogo de películas de {self.nombreCatalogo}.")
         
     #Permite mostrar todas las opciones disponibles para trabajar en el catálogo de películas
     def mostrar_menu_opciones(self):
@@ -82,15 +103,16 @@ class CatalogoPelicula:
                         "1) Agregar película\n"
                         "2) Mostrar Catálogo de películas \n"
                         "3) Eliminar Catálogo de películas \n"
-                        "4) Salir \n"
+                        "4) Cambiar otro Catálogo de películas \n"
+                        "5) Salir \n"
                         "Tu opción: "))
-            if opcion == 1 or opcion == 2 or opcion == 3 or opcion == 4:
+            if opcion == 1 or opcion == 2 or opcion == 3 or opcion == 4 or opcion == 5:
                 return opcion
             else:
-                print("Error: Solo se permite un número del 1 al 4.")
+                print("Error: Solo se permite un número del 1 al 5.")
                 return CatalogoPelicula.mostrar_menu_opciones()  
         except ValueError:
-            print("Error: Solo se permite un número del 1 al 4.")
+            print("Error: Solo se permite un número del 1 al 5.")
             return CatalogoPelicula.mostrar_menu_opciones()
     
     # Permite mostrar las opciones del menú para el catálogo de película
@@ -105,6 +127,10 @@ class CatalogoPelicula:
                 CatalogoPelicula.listar_peliculas(self)
             elif opcion == 3:
                 CatalogoPelicula.eliminar_catalogo(self)
+                break
+            elif opcion == 4:
+                CatalogoPelicula.cambiar_catalogo(self)
+                break
             else:
                 break
 
